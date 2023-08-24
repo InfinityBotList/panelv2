59d944814901ce8491a24dfb348d28d424d114c0
cheesycod <cheesycod@outlook.com>
2023-08-24 20:06:41 +0530

    fixes

--- src/routes/panel/+page.svelte
+++ src/routes/panel/+page.svelte
@@ -2,12 +2,6 @@
 	import { panelState } from "$lib/panelData";
 	import ButtonLink from "../../components/ButtonLink.svelte";
 	import GreyText from "../../components/GreyText.svelte";
-	import ListItem from "../../components/ListItem.svelte";
-	import UnorderedList from "../../components/UnorderedList.svelte";
-	import InfoPane from "../../components/pane/InfoPane.svelte";
-	import PaneContent from "../../components/pane/PaneContent.svelte";
-	import PaneData from "../../components/pane/PaneData.svelte";
-	import PaneWrapper from "../../components/pane/PaneWrapper.svelte";
 
     interface QuickAction {
         name: string;
@@ -32,54 +26,25 @@
     }
 </script>
 
-<PaneWrapper>
-    <InfoPane title="Permissions" description="Your Permissions?">
-        <PaneData>            
-                <UnorderedList>
-                    {#if $panelState?.userPerms?.owner}
-                        <ListItem>Owner (owner)</ListItem>
-                    {/if}
-                    {#if $panelState?.userPerms?.hadmin}
-                        <ListItem>Head Staff Manager (hadmin)</ListItem>
-                    {/if}
-                    {#if $panelState?.userPerms?.admin}
-                        <ListItem>Staff Manager (admin)</ListItem>
-                    {/if}
-                    {#if $panelState?.userPerms?.iblhdev}
-                        <ListItem>Head Developer (iblhdev)</ListItem>
-                    {/if}
-                    {#if $panelState?.userPerms?.ibldev}
-                        <ListItem>Developer (ibldev)</ListItem>
-                    {/if}
-                    {#if $panelState?.userPerms?.staff}
-                        <ListItem>Staff (staff)</ListItem>
-                    {/if}
-                </UnorderedList>
-        </PaneData>
-    </InfoPane>
+<h1 class="text-3xl font-semibold">Hey there {$panelState?.userDetails?.display_name} ({$panelState?.userDetails?.username})</h1>
 
-    <PaneContent>
-        <h1 class="text-3xl font-semibold">Hey there {$panelState?.userDetails?.display_name} ({$panelState?.userDetails?.username})</h1>
+<h2 class="text-2xl font-semibold mt-24">Quick Actions</h2>
 
-        <h2 class="text-2xl font-semibold mt-24">Quick Actions</h2>
-
-        <div class="mt-4">
-            {#each quickActions as action}
-                <div class="flex flex-row items-center justify-between">
-                    <div class="flex flex-col">
-                        <h2 class="text-lg font-semibold">{action.name}</h2>
-                        <GreyText>{action.description}</GreyText>
-                    </div>
-                    <div class="flex flex-row items-center">
-                        <ButtonLink
-                            link={action.link}
-                        >
-                            {action.name}
-                        </ButtonLink>
-                    </div>
-                </div>
-                <hr class="my-4" />
-            {/each}    
+<div class="mt-4">
+    {#each quickActions as action}
+        <div class="flex flex-row items-center justify-between">
+            <div class="flex flex-col">
+                <h2 class="text-lg font-semibold">{action.name}</h2>
+                <GreyText>{action.description}</GreyText>
+            </div>
+            <div class="flex flex-row items-center">
+                <ButtonLink
+                    link={action.link}
+                >
+                    {action.name}
+                </ButtonLink>
+            </div>
         </div>
-    </PaneContent>
-</PaneWrapper>
+        <hr class="my-4" />
+    {/each}    
+</div>
