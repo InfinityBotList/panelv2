ae3d76f7dc3c1b6e8ded4ae62840e72d4be30203
cheesycod <cheesycod@outlook.com>
2023-10-07 18:03:34 +0530

    fix: no more partner.image_type

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -15,9 +15,7 @@
 <div class="rounded-lg p-4 bot_card bg-themable-600/50 shadow-white/50 text-white">
 	<div class="rounded-lg p-3 bg-black card_banner">
 		<div class="flex justify-center items-center">
-			<span class="rounded-full w-10 h-10">
-				<slot name="image" />
-			</span>
+			<slot name="image" class="rounded-md w-10 h-10" />
 			<div class="items-center pl-4 mt-1 text-2xl">
 				<span class="inline-block text-2xl font-semibold text-white line-clamp-1 tracking-tight">
 					<slot name="display-name" />
