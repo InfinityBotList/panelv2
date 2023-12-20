31adf4bf717fd898184ac3e472b5200acaddb539
selectdev <66577268+selectdev@users.noreply.github.com>
2023-12-20 00:46:14 -0600

    Update ListItem.svelte

--- src/components/ListItem.svelte
+++ src/components/ListItem.svelte
@@ -1,3 +1,7 @@
-<li class="mb-2 border-gray-900 break-all break-words">
+<script lang="ts">
+	export let class: string | null = "";
+</script>
+
+<li class="mb-2 border-gray-900 break-all break-words {class}">
 	<slot />
 </li>
